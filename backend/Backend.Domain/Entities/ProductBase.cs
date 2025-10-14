62430b78d4b5029f40d680f027929cf56cf99294
Ivaylo Georgiev <120925473+Ivaylo2201@users.noreply.github.com>
2025-10-14 22:27:58 +0300

    Added quantity field to product and property to check availability

--- backend/Backend.Domain/Entities/ProductBase.cs
+++ backend/Backend.Domain/Entities/ProductBase.cs
@@ -9,10 +9,12 @@ public abstract class ProductBase
     public required string ImagePath { get; init; }
     public required double InitialPrice { get; init; }
     public int DiscountPercentage { get; init; }
+    public required int Quantity { get; set; }
     
     public ICollection<Item> Items { get; init; } = [];
     public ICollection<Review> Reviews { get; init; } = [];
     public ICollection<User> LikedBy { get; init; } = [];
 
     public double Price => InitialPrice * (1 - DiscountPercentage / 100.0);
+    public bool IsOutOfStock => Quantity == 0;
 }
