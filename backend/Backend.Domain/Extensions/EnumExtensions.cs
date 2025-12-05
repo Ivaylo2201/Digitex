14c88304e18f50dd3dde4aa468541bf09b77ab02
Ivaylo Georgiev <ivaylo.developer@gmail.com>
2025-12-05 13:44:53 +0200

    Add EnumExtensions for retrieving enum member values

---
+++ backend/Backend.Domain/Extensions/EnumExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Backend.Domain.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetEnumMemberValue<T>(this T enumValue) where T : Enum
+    {
+        var member = typeof(T).GetMember(enumValue.ToString()).FirstOrDefault();
+        var attr = member?.GetCustomAttribute<EnumMemberAttribute>();
+        return attr?.Value ?? enumValue.ToString();
+    }
+}
